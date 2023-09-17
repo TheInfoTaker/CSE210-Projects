dd93a7105140d7a3b9749c7a34c98e4bf51bd29c
TheInfoTaker <122415302+TheInfoTaker@users.noreply.github.com>
2023-09-16 21:44:45 -0600

    Wrote code...

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int guesses = 1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+                guesses = guesses + 1;
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+                guesses = guesses + 1;
+            } 
+            else
+            {
+                Console.WriteLine($"That's it! You made {guesses} guesses.");
+                string guesses2 = guesses.ToString();
+                Console.WriteLine($"You guessed {guesses2} times.");
+            }
+        }
     }
 }
